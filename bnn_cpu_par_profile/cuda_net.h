06234c73f2e801746a5c282091447fb62d296123
Leonard David <leonard.davidbereholschi@gmail.com>
2022-02-09 13:57:30 +0100

    added profile V

--- bnn_cpu_par_profile/cuda_net.h
+++ bnn_cpu_par_profile/cuda_net.h
@@ -13,17 +13,17 @@
 //   return layer3_step_cuda(layer1, layer2);
 // }
 
-// float layer4_conv(unsigned long long * layer1, signed short * layer2){
-//   return layer4_conv_cuda(layer1, layer2);
-// }
+float layer4_conv(unsigned long long * layer1, signed short * layer2){
+  return layer4_conv_cuda(layer1, layer2);
+}
 
 // float layer5_maxpool(signed short * layer1, signed short * layer2){
 //   return layer5_maxpool_cuda(layer1, layer2);
 // }
 
-// float layer8_gemm(unsigned long long * layer1, signed short * layer2){
-//   return layer8_gemm_cuda(layer1, layer2);
-// }
+float layer8_gemm(unsigned long long * layer1, signed short * layer2){
+  return layer8_gemm_cuda(layer1, layer2);
+}
 
 // float layer10_gemm(unsigned long long * layer1, signed short * layer2){
 //   return layer10_gemm_cuda(layer1, layer2);

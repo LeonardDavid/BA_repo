36363e5bfdd01e080875396afa24b35c656d8865
Leonard David <leonard.davidbereholschi@gmail.com>
2022-02-14 14:45:51 +0100

    update

--- bnn_cpu_par_profile/cuda_kernel.cu
+++ bnn_cpu_par_profile/cuda_kernel.cu
@@ -2470,7 +2470,7 @@ __global__ void layer1_conv_kernel(unsigned char *d_cuda_layer_0_output, float *
 
     // https://github.com/ULHPC/tutorials/blob/devel/cuda/exercises/convolution/LoG_gpu_solution.cu
 
-    int N = 28, kernel_size = 3;
+    const int N = 28, kernel_size = 3;
 
     int tid = threadIdx.x; // = h
     int bid = blockIdx.y;  // = w
@@ -2488,8 +2488,21 @@ __global__ void layer1_conv_kernel(unsigned char *d_cuda_layer_0_output, float *
     //idx global index (all blocks) of the image pixel 
     int idx = iy*N +ix;
 
-    __shared__ float s_bias[NR_NEURONS];
-    s_bias[m] = d_layer_1_bias[m];
+    const int c = 1; // channels
+    __shared__ float s_weight[kernel_size * kernel_size * c];
+    for (int kH = 0; kH < kernel_size; kH++){
+        int iH = h * 1 + kH - 1;
+        if (iH >= 0 && iH < 28) {
+            for (int kW = 0; kW < kernel_size; kW++){
+                int iW = w * 1 + kW - 1;
+                if (iW >= 0 && iW < 28) {
+                    for (int c = 0; c < 1; c++) {
+                        s_weight[index3D_cuda(kH,kW,c,3,1)] = d_cuda_layer_1_weight[index4D_cuda(kH,kW,c,m,3,1,64)];
+                    }
+                }
+            }
+        }
+    }
 
     __syncthreads();
 
@@ -2497,7 +2510,7 @@ __global__ void layer1_conv_kernel(unsigned char *d_cuda_layer_0_output, float *
     if(tid < N){
         if(b<BATCH_SIZE){
             if(m<NR_NEURONS) {
-                d_cuda_layer_1_output[index4D_cuda(b,h,w,m,28,28,64)] = s_bias[m];
+                d_cuda_layer_1_output[index4D_cuda(b,h,w,m,28,28,64)] = d_layer_1_bias[m];
             }
         }
     }
@@ -2516,7 +2529,8 @@ __global__ void layer1_conv_kernel(unsigned char *d_cuda_layer_0_output, float *
                             for (int c = 0; c < 1; c++) {
                                 if(m<NR_NEURONS) {
                                     // atomicAdd(&d_cuda_layer_1_output[index4D_cuda(b,bid,tid,m,28,28,64)], d_cuda_layer_1_weight[index4D_cuda(kH,kW,c,m,3,1,64)] * d_cuda_layer_0_output[index4D_cuda(b,iH,iW,c,28,28,1)]);
-                                    d_cuda_layer_1_output[index4D_cuda(b,h,w,m,28,28,64)] += d_cuda_layer_1_weight[index4D_cuda(kH,kW,c,m,3,1,64)] * d_cuda_layer_0_output[index4D_cuda(b,iH,iW,c,28,28,1)];
+                                    // d_cuda_layer_1_output[index4D_cuda(b,h,w,m,28,28,64)] += d_cuda_layer_1_weight[index4D_cuda(kH,kW,c,m,3,1,64)] * d_cuda_layer_0_output[index4D_cuda(b,iH,iW,c,28,28,1)];
+                                    d_cuda_layer_1_output[index4D_cuda(b,h,w,m,28,28,64)] += s_weight[index3D_cuda(kH,kW,c,3,1)] * d_cuda_layer_0_output[index4D_cuda(b,iH,iW,c,28,28,1)];
                                 }
                             }
                         }

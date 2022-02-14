08bdedd9eb2225cd3dc3525838b3445923878512
Leonard David <leonard.davidbereholschi@gmail.com>
2022-02-14 20:29:48 +0100

    update

--- bnn_cpu_par_profile/main.cpp
+++ bnn_cpu_par_profile/main.cpp
@@ -154,41 +154,41 @@ int main() {
     printf("\n");
 
     // for profiling Layers
-    float l1_time = std::get<5>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l2_time = std::get<6>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l3_time = std::get<7>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l4_time = std::get<8>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l5_time = std::get<9>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l6_time = std::get<10>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l8_time = std::get<11>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l9_time = std::get<12>(results)/1000000.0f; // ns / 1e6 -> ms
-    float l10_time = std::get<13>(results)/1000000.0f; // ns / 1e6 -> ms
+    float l1_time = std::get<5>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l2_time = std::get<6>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l3_time = std::get<7>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l4_time = std::get<8>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l5_time = std::get<9>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l6_time = std::get<10>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l8_time = std::get<11>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l9_time = std::get<12>(results)/1000000000.0f; // ns / 1e9 -> s
+    float l10_time = std::get<13>(results)/1000000000.0f; // ns / 1e9 -> s
 
-    float l1_ktime = std::get<14>(results)/1.0f; // ms -> ms
-    float l2_ktime = std::get<15>(results)/1.0f; // ms -> ms
-    float l4_ktime = std::get<16>(results)/1.0f; // ms -> ms
-    float l5_ktime = std::get<17>(results)/1.0f; // ms -> ms
-    float l8_ktime = std::get<18>(results)/1.0f; // ms -> ms
-    float l10_ktime = std::get<19>(results)/1.0f; // ms -> ms
+    float l1_ktime = std::get<14>(results)/1000.0f; // ms / 1e3 -> s
+    float l2_ktime = std::get<15>(results)/1000.0f; // ms / 1e3 -> s
+    float l4_ktime = std::get<16>(results)/1000.0f; // ms / 1e3 -> s
+    float l5_ktime = std::get<17>(results)/1000.0f; // ms / 1e3 -> s
+    float l8_ktime = std::get<18>(results)/1000.0f; // ms / 1e3 -> s
+    float l10_ktime = std::get<19>(results)/1000.0f; // ms / 1e3 -> s
 
-    float malloc_time = std::get<20>(results)/1000000.0f; // ns / 1e6 -> ms
-    float cpy_time = std::get<21>(results)/1000000.0f; // ns / 1e6 -> ms
+    float malloc_time = std::get<20>(results)/1000000000.0f; // ns / 1e9 -> s
+    float cpy_time = std::get<21>(results)/1000000000.0f; // ns / 1e9 -> s
 
     float sum_l = l1_time + l2_time + l3_time + l4_time + l5_time + l6_time + l8_time + l9_time + l10_time;
     float sum_kl = l1_ktime + l2_ktime + l4_ktime + l5_ktime + l8_ktime + l10_ktime;
 
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n %-50s%-10s %-5.2f [ms]\n %-50s%-10s %-5.2f [ms]\n", "Layer 1 time:", l1_time, "Ratio:", (l1_time/sum_l)*100, "kernel:", l1_ktime, "kRatio:", (l1_ktime/sum_kl)*100,
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n %-50s%-10s %-5.2f [s]\n %-50s%-10s %-5.2f [s]\n", "Layer 1 time:", l1_time, "Ratio:", (l1_time/sum_l)*100, "kernel:", l1_ktime, "kRatio:", (l1_ktime/sum_kl)*100,
                                                                                 "","=> malloc:", malloc_time, "","=> copy: ", cpy_time);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n", "Layer 2 time:", l2_time, "Ratio:", (l2_time/sum_l)*100, "kernel:", l2_ktime, "kRatio:", (l2_ktime/sum_kl)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f%\n", "Layer 3 time:", l3_time, "Ratio:", (l3_time/sum_l)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n", "Layer 4 time:", l4_time, "Ratio:", (l4_time/sum_l)*100, "kernel:", l4_ktime, "kRatio:", (l4_ktime/sum_kl)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n", "Layer 5 time:", l5_time, "Ratio:", (l5_time/sum_l)*100, "kernel:", l5_ktime, "kRatio:", (l5_ktime/sum_kl)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f%\n", "Layer 6 time:", l6_time, "Ratio:", (l6_time/sum_l)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n", "Layer 8 time:", l8_time, "Ratio:", (l8_time/sum_l)*100, "kernel:", l8_ktime, "kRatio:", (l8_ktime/sum_kl)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f%\n", "Layer 9 time:", l9_time, "Ratio:", (l9_time/sum_l)*100);
-    printf("%-15s %-10.2f [ms], %-10s %-5.2f% => %-5s %-5.2f [ms] %-10s %-5.2f%\n", "Layer 10 time:", l10_time, "Ratio:", (l10_time/sum_l)*100, "kernel:", l10_ktime, "kRatio:", (l10_ktime/sum_kl)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n", "Layer 2 time:", l2_time, "Ratio:", (l2_time/sum_l)*100, "kernel:", l2_ktime, "kRatio:", (l2_ktime/sum_kl)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f%\n", "Layer 3 time:", l3_time, "Ratio:", (l3_time/sum_l)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n", "Layer 4 time:", l4_time, "Ratio:", (l4_time/sum_l)*100, "kernel:", l4_ktime, "kRatio:", (l4_ktime/sum_kl)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n", "Layer 5 time:", l5_time, "Ratio:", (l5_time/sum_l)*100, "kernel:", l5_ktime, "kRatio:", (l5_ktime/sum_kl)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f%\n", "Layer 6 time:", l6_time, "Ratio:", (l6_time/sum_l)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n", "Layer 8 time:", l8_time, "Ratio:", (l8_time/sum_l)*100, "kernel:", l8_ktime, "kRatio:", (l8_ktime/sum_kl)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f%\n", "Layer 9 time:", l9_time, "Ratio:", (l9_time/sum_l)*100);
+    printf("%-15s %-10.2f [s], %-10s %-5.2f% => %-5s %-5.2f [s] %-10s %-5.2f%\n", "Layer 10 time:", l10_time, "Ratio:", (l10_time/sum_l)*100, "kernel:", l10_ktime, "kRatio:", (l10_ktime/sum_kl)*100);
     printf("\n");
-    printf("%-15s %.2f [ms]\n%-15s %.2f [ms]\n", "Total time:", sum_l, "Total ktime:", sum_kl);
+    printf("%-15s %.2f [s]\n%-15s %.2f [s]\n", "Total time:", sum_l, "Total ktime:", sum_kl);
 
     return 0;
 }

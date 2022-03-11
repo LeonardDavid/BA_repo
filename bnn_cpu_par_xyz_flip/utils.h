b622f0b9c7f9c746df18f7915c88614b4d7e8870
Leonard David <leonard.davidbereholschi@gmail.com>
2022-03-11 15:39:16 +0100

    update

--- bnn_cpu_par_xyz_flip/utils.h
+++ bnn_cpu_par_xyz_flip/utils.h
@@ -1,4 +1,4 @@
-#define BATCH_SIZE 5000
+#define BATCH_SIZE 2
 #define NR_NEURONS 64   // for the fashion net, this value is constant, but for others (i.e. cifar10) it depends on the layer
 #define IMG_HEIGHT 28   // original input image
 #define IMG_WIDTH 28    // original input image
